8b400ff665267e1104706898990fc10c9b265ed6
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2023-09-19 16:14:36 -0500

    Organize tests on test suites

--- src/app/calculator.spec.ts
+++ src/app/calculator.spec.ts
@@ -1,26 +1,32 @@
 import { Calculator } from './calculator';
 
 // fdescribe() to focus on this set of tests 
-describe('Tests for Calculator', () => {
-  it('#multiply should return 9', () => {
-    // Arrange
-    const calculator = new Calculator();
-    // Act
-    const result = calculator.multiply(3, 3);
-    // Assert
-    expect(result).toEqual(9);
+describe('Tests for Calculator class', () => {
+  describe('Tests for multiply()', () => {
+    it('should return 9', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act
+      const result = calculator.multiply(3, 3);
+      // Assert
+      expect(result).toEqual(9);
+    });
   });
 
-  it('#divide should return two on each scenario', () => {
-    const calculator = new Calculator();
-
-    expect(calculator.divide(6, 3)).toEqual(2);
-    expect(calculator.divide(12, 6)).toEqual(2);
-  });
-
-  it('#divide should return null if divided by 0', () => {
-    const calculator = new Calculator();
-
-    expect(calculator.divide(6, 0)).toBeNull();
+  describe('Tests for divide()', () => {
+    it('should return two on each scenario', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act & Assert
+      expect(calculator.divide(6, 3)).toEqual(2);
+      expect(calculator.divide(12, 6)).toEqual(2);
+    });
+  
+    it('should return null if divided by 0', () => {
+      // Arrange
+      const calculator = new Calculator();
+      // Act & Assert
+      expect(calculator.divide(6, 0)).toBeNull();
+    });
   });
 });
