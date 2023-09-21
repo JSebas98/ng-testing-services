c20f113f681006aa9d36c4e7676d35a37e5ba31d
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2023-09-21 12:23:22 -0500

    Add Pico css and components for testing

--- src/app/calculator.ts
+++ src/app/calculator.ts
@@ -3,7 +3,14 @@ export class Calculator {
     return a * b;
   }
 
-  public divide(a: number, b: number): number | null {
+  /**
+* Divides two numbers.
+* 
+* @param {number} a - The dividend number.
+* @param {number} b - The divisor number.
+* @returns {number | null} The quotient of a and b if b is not 0, otherwise null.
+*/
+public divide(a: number, b: number): number | null {
     if (b === 0) {
       return null;
     }
