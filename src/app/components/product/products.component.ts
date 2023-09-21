37d18a5c838b6a4aeb1b11e221335d3dedc54412
Sebastian Beltran <sebastian.beltran@jalasoft.com>
2023-09-21 13:36:25 -0500

    Add product service

--- src/app/components/product/products.component.ts
+++ src/app/components/product/products.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  
+  products: Product[] = [];
+
+  constructor(
+    private productsService: ProductsService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+
+  getAllProducts(): void { 
+    this.productsService.getAllSimple()
+      .subscribe({
+        next: (products: Product[]) => {
+          this.products = products;
+        }
+      });
+  }
 
 }
